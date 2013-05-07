7228ddd75afbff3f9dc68f7634c3f05930eb69c2
じゃむ <jamjam@tom.sfc.keio.ac.jp>
2013-05-07 16:49:18 +0900

    更新

--- JavaCollection/src/Fourth/Crawler.java
+++ JavaCollection/src/Fourth/Crawler.java
@@ -5,6 +5,56 @@
 
 package Fourth;
 
-public class Crawler {
+import java.util.Set;
 
+import javax.xml.soap.Node;
+
+import org.w3c.dom.NodeList;
+
+/*
+ * Crawlerクラス。
+ * ページから文字列を引っ張りだしてデータベースの型に当てはめるクラス。
+ */
+public class Crawler{
+	public Crawler(String dbname)
+	{
+		init();
+	}
+
+	private void init()
+	{
+	}
+
+	public void addtoindex(String url, NodeList soup)
+	{
+		System.out.println("Indexing " + url);
+	}
+	
+	public String gettextonly(Node soup)
+	{
+		return null;
+	}
+	
+	public String gettextonly(NodeList children)
+	{
+		return null;
+	}
+	
+	public Boolean isindexed(String url)
+	{
+		return null;
+	}
+	
+	public void addlinkref(String urlFrom,String urlTo, String linkText)
+	{
+	}
+	
+	public void crawl(Set<String> pages, int depth)
+	{
+	}
+	
+	public void createindextables()
+	{
+	}
 }
+
